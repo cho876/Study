b0c907a5ab0d04206d39199287fef9cb577c3560
kwonnee <kwonnee@nexon.co.kr>
2021-12-16 10:40:12 +0900

    Prac 02. List 데이터 전송 및 table로 출력하기

--- thymeleafStudy/src/main/java/com/skcho/thymeleafStudy/controller/StudyController.java
+++ thymeleafStudy/src/main/java/com/skcho/thymeleafStudy/controller/StudyController.java
@@ -1,5 +1,8 @@
 package com.skcho.thymeleafStudy.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -7,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.skcho.thymeleafStudy.constant.ReqMap;
+import com.skcho.thymeleafStudy.vo.StudentVo;
 
 @Controller
 @RequestMapping(ReqMap.MAIN_PATH)
@@ -17,10 +21,30 @@ public class StudyController {
 	// Prac 01. model로 넘겨 Hello World 찍기
 	@RequestMapping(value="/prac1")
 	public String prac1(Model model) {
-		logger.info("in Index");
+		logger.info("prac1 called");
 		
 		model.addAttribute("title", "Hello World");
 		
 		return "prac1/index";
+	}
+	
+	// Prac 02. List 데이터를 넘겨 table로 출력하기
+	@RequestMapping(value="/prac2")
+	public String prac2(Model model) {
+		logger.info("prac2 called");
+		
+		List<StudentVo> studentList = new ArrayList<>();
+		
+		StudentVo studentVo1 = new StudentVo("12345","kwonnee1","28");
+		StudentVo studentVo2 = new StudentVo("67890","kwonnee2","29");
+		StudentVo studentVo3 = new StudentVo("00000","kwonnee3","30");
+		
+		studentList.add(studentVo1);
+		studentList.add(studentVo2);
+		studentList.add(studentVo3);
+		
+		model.addAttribute("studentList",studentList);
+		
+		return "prac2/index";
 	}
 }
---
+++ thymeleafStudy/src/main/java/com/skcho/thymeleafStudy/vo/StudentVo.java
@@ -0,0 +1,13 @@
+package com.skcho.thymeleafStudy.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class StudentVo {
+	
+	private String id;
+	private String name;
+	private String age;
+}
