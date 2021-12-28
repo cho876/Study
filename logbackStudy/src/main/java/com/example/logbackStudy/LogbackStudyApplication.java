23d8fa6239fd95afe9b3ac9b118f2526ee705550
cho876 <weapon114@naver.com>
2021-12-28 21:36:09 +0900

    springboot logback 설정하기

---
+++ logbackStudy/src/main/java/com/example/logbackStudy/LogbackStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.logbackStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogbackStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogbackStudyApplication.class, args);
+	}
+
+}
