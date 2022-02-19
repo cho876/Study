1cb679feba58b61a3f409b9481cec13a04245740
cho876 <weapon114@naver.com>
2022-02-19 15:16:33 +0900

    springboot에 ssl적용하기

---
+++ sslStudy/src/test/java/com/example/sslStudy/SslStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sslStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SslStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
